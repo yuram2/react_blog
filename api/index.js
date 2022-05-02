01a12273a16745c9c5c3a96d71f88112b36d035a
yuram2 <lur0872@gmail.com>
2022-05-02 13:12:03 +0900

    mongodb

---
+++ api/index.js
@@ -0,0 +1,19 @@
+const express = require("express"); //라이브 서버
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require('mongoose');
+const authRoute = require("./routes/auth");
+
+dotenv.config();
+app.use(express.json());
+
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(console.log("몽고DB랑 연결이 되었습니다."))
+    .catch((err) => console.log(err));
+
+    app.use("/api/auth", authRoute);
+
+app.listen(5000, () => { //port번호
+    console.log("Backend is running ");
+});
